ca79f23cdfd61644709634524fc0cf4891b6ab2a
Вячеслав Титов <vcslvttv@gmail.com>
2020-06-10 19:20:53 +0500

    Custom Table View Cell

---
+++ My places/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  My places
+//
+//  Created by vcslvttv on 10/06/2020.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
