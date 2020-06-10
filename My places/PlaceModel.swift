4773f2c18694e73d823861010138497f1f5cd0bc
Вячеслав Титов <vcslvttv@gmail.com>
2020-06-10 19:37:20 +0500

    Work with model

---
+++ My places/PlaceModel.swift
@@ -0,0 +1,35 @@
+//
+//  PlaceModel.swift
+//  My places
+//
+//  Created by vcslvttv on 10/06/2020.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import UIKit
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+        static let restaurantNames = [
+            "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+            "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+            "Speak Easy", "Morris Pub", "Вкусные истории",
+            "Классик", "Шок", "Бочка"
+        ]
+    
+        static func getPlaces() -> [Place] {
+        
+            var places = [Place]()
+        
+            for place in restaurantNames {
+                places.append(Place(name: place, location: "Тагил", type: "Restaurant", image: place))
+            }
+        
+            return places
+    }
+}
