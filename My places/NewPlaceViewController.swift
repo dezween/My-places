6217d89ef731b177165008fcddff2a3f8ba8fa94
Вячеслав Титов <vcslvttv@gmail.com>
2020-06-15 22:32:35 +0500

    Refactor Code to Use DB

--- My places/NewPlaceViewController.swift
+++ My places/NewPlaceViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
-    var newPlace = Place()
     var imageIsChanged = false
     
     @IBOutlet weak var placeImage: UIImageView!
@@ -23,15 +22,8 @@ class NewPlaceViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        DispatchQueue.main.async {
-            self.newPlace.savePlaces()
-        }
-        
-        
         tableView.tableFooterView = UIView()
-        
         saveButton.isEnabled = false
-        
         placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
     }
     
@@ -79,7 +71,14 @@ class NewPlaceViewController: UITableViewController {
             image = #imageLiteral(resourceName: "imagePlaceholder")
         }
         
-//        newPlace = Place(name: placeName.text!, location: placeLocation.text, type: placeType.text, image: image, restaurantImage: nil)
+        let imageData = image?.pngData()
+        
+        let newPlace = Place(name: placeName.text!,
+                             location: placeLocation.text,
+                            type: placeType.text,
+                            imageData: imageData)
+        
+        StorageManager.saveObject(newPlace)
     }
     
     @IBAction func cancelAction(_ sender: Any) {
--- My places/PlaceModel.swift
+++ My places/PlaceModel.swift
@@ -15,32 +15,11 @@ class Place: Object {
     @objc dynamic var type: String?
     @objc dynamic var imageData: Data?
    
-    
-        let restaurantNames = [
-            "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
-            "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
-            "Speak Easy", "Morris Pub", "Вкусные истории",
-            "Классик", "Шок", "Бочка", "Love&Life"
-        ]
-    
-        func savePlaces() {
-        
-        
-            for place in restaurantNames {
-                
-                let image = UIImage(named: place)
-                guard let imageData = image?.pngData() else { return }
-                
-                
-                let newPlace = Place()
-                
-                newPlace.name = place
-                newPlace.location = "Tagil"
-                newPlace.type = "Restaurant"
-                newPlace.imageData = imageData
-                
-                StorageManager.saveObject(newPlace)
-            }
-        
+    convenience init(name: String, location: String?, type: String?, imageData: Data?) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.type = type
+        self.imageData = imageData
     }
 }
