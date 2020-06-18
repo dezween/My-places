4e129ac7a22a47ca61c1dc455619e6b2713282d7
Вячеслав Титов <vcslvttv@gmail.com>
2020-06-18 22:59:34 +0500

    Map View Controller

---
+++ My places/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  My places
+//
+//  Created by vcslvttv on 18/06/2020.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+
+}
